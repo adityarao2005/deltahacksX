8d8e25d1d4920eebf9e584e47c7d1d16c7ebf9c0
taeyeonha <taeyeonha10@gmail.com>
2024-01-14 00:02:40 -0500

    Added Tools

--- quake-guard/quake-guard-frontend/src/app/donate/page.tsx
+++ quake-guard/quake-guard-frontend/src/app/donate/page.tsx
@@ -1,27 +1,33 @@
 import React from "react";
+import user_icon from "../Icons/user.jpg"
+import email_icon from "../Icons/email.jpg"
+import password_icon from "../Icons/password.jpg"
 
 export default function AboutPage() {
-	return (
-		<div className="border border-black rounded">
-			<div className="make-box">
-				<h1>Sign up</h1>
-				<form>
-					<div className="input-group">
-						<div className="input-field">
-							<i className="fa-solid fa_user"></i>
-							<input type="text" placeholder="Name" />
-						</div>
-						<div className="input-field">
-							<i className="fa-solid fa_envelope"></i>
-							<input type="email" placeholder="Email" />
-						</div>
-						<div className="input-field">
-							<i className="fa-solid fa_lock"></i>
-							<input type="password" placeholder="Password" />
-						</div>
-					</div>
-				</form>
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign up</div>
+        <div className="underline"></div>
+      </div>
+			<div className="inputs">
+				<div className= "img src = '/user.jpg'">
+					<input type="text" placeholder="Name"/>
+				</div>
+				<div className="input">
+					{/*<img src={email_icon} alt="" />*/}
+					<input type="email" placeholder="Email"/>
+				</div>
+				<div className="input">
+					{/*<img src={password_icon} alt="" />*/}
+					<input type="password" placeholder="Password"/>
+				</div>
 			</div>
-		</div>
-	);
+			<div className="forgot-password">Lost Password? <span>Click Here!</span></div>
+			<div className="submit">
+				<div className="submit">Sign up</div>
+				<div className="submit">Login</div>
+			</div>
+    </div>
+  )
 }
