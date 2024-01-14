586e003f16427935b5e515b9439eed08994dfaaf
taeyeonha <taeyeonha10@gmail.com>
2024-01-14 06:49:02 -0500

    About Us page

--- quake-guard/quake-guard-frontend/src/app/about/page.tsx
+++ quake-guard/quake-guard-frontend/src/app/about/page.tsx
@@ -8,7 +8,7 @@ export default function AboutPage() {
 				<h1 className='text-3xl font-bold ml-24 mb-4 p-3 underline'>
 					The Solution
 				</h1 > 
-				<p className ='ml-24 p-3 pb-4 mr-16'><i><strong>QuakeGuard</strong></i> relies on <strong>Earthquake API's</strong>, <strong>Pandas analysis</strong> and a <strong>machine learning model</strong> to accurately tell a user what is the chances that an earthquake will occur in their area, it's strength and the expected date it will happen.</p>
+				<p className ='ml-24 p-3 pb-4 mr-16'><i><strong>QuakeGuard</strong></i> relies on <strong>Earthquake APIs</strong>, <strong>Pandas analysis</strong> and a <strong>machine learning model</strong> to accurately tell the user the chances that an earthquake will occur in their area, it's strength, and the expected date it will happen.</p>
 				<div>
 					<p className="font-bold ml-24 p-3 pb-8">So... How are we <u>DIFFERENT</u>???</p>
 				</div>
@@ -17,7 +17,7 @@ export default function AboutPage() {
 						1. AI and Machine Learning Powered
 					</p>
 					<p className='ml-24 p-2 pb-8 mr-16'>
-						Uses AI and Machine Learning predicting earthquakes with higher accuracy, enabling timely and informed decisions by authorities and communities!
+						Uses <strong>AI</strong> and <strong>Machine Learning</strong> predicting earthquakes with higher accuracy, enabling timely and informed decisions by authorities and communities!
 					</p>
 				</div>
 				<div>
@@ -25,13 +25,13 @@ export default function AboutPage() {
 						2. Global Community & Support
 					</p>
 					<p className='ml-24 p-2 pb-8 mr-16'>
-						Unlike traditional models, QuakeGuard involves an active amount of users to allocatively rationing resources to high-risk areas, helping the ones who most need it!
+						Unlike traditional models, <i><strong>QuakeGuard</strong></i> involves an active amount of users to allocatively ration resources to high-risk areas, helping the ones who most need it!
 					</p>
 					<p className='font-bold ml-24 px-2'>
 						3. Hollistic Analysis
 					</p>
 					<p className='ml-24 p-2 mr-16'>
-						Our analysis considers langitude and latitude, depth of earthquake, date and its Richter rating from a diverse dataset providing more accurate outputs and more comprehensive summary results! 
+						Our analysis considers langitude and latitude, depths of earthquakes, dates and its Richter ratings from a diverse <strong>dataset</strong> providing more accurate outputs and comprehensive summary results! 
 					</p>
 				</div>
 			</div>
@@ -41,7 +41,7 @@ export default function AboutPage() {
 						The Product
 					</h1>
 					<p className='py-4 leading-relaxed ml-6 mr-24'>
-						As rising condo and urban living is increasing, dangers due to earthquakes continue to rise. As such the need for a proactive and data driven earthquake risk prediction has never been more critical. Our initiative, <strong><i>QuakeGuard</i></strong>, harnesses <strong>Machine Learning</strong> capabilities to revolutionize earthquake risk assesment. QuakeGuard is committed in providing information to empowering authorities, conmmunities and first responders with actionable insights for resource allocation and prevention by geographical, longitudinal and latitudinal analysis.  
+						As condos and urban living are increasing, dangers due to earthquakes continue to rise. As such the need for a proactive and data driven earthquake risk prediction has never been more critical. Our initiative, <strong><i>QuakeGuard</i></strong>, harnesses <strong>Machine Learning</strong> capabilities to revolutionize earthquake risk assesment. <i><strong>QuakeGuard</strong></i> is committed in providing information to empowering authorities, commmunities and first responders with actionable insights for resource allocation and prevention by geographical, longitudinal and latitudinal analysis.  
 					</p>
 				</div>
 				<div className='pb-4 p-4 leading-relaxed'>
@@ -49,7 +49,7 @@ export default function AboutPage() {
 						The Inspiration
 					</h1>
 					<p className='py-4 leading-relaxed ml-6 mr-24'>
-						Our inspiration for this project rose from the devestating earthquakes that struck Turkey recently. As environmental enthusiasts, we knew we wanted to help. Our goal was to make something new and exciting that combined elements we were familiar with such as <strong>Node.js</strong>, <strong>React and TailwindCSS</strong>, as well as elements we wanted to learn more about such as <strong>ML</strong>. And so began our journey to create an extraordinary hack that not only adresses a current problem but also paves the way for a brighter future. 
+						Our inspiration for this project rose from the devestating earthquakes that struck Turkey recently. As environmental enthusiasts, we knew we wanted to help. Our goal was to make something new and exciting that combined elements we were familiar with such as <strong>Node.js</strong>, <strong>React.js and TailwindCSS</strong>, as well as elements we wanted to learn more about such as <strong>ML</strong>. And so began our journey to create an extraordinary hack that not only addresses a current problem but also paves the way for a brighter future. 
 					</p>
 				</div>
 				<div className='bg-cover mt-20 mr-20 h-80 rounded-2xl bg-[url("/mockup3.jpg")] p-3 '></div>
