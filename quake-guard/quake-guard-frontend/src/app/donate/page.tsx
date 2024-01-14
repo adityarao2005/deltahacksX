31c6e7061a1d48de6b1ac5e925b2553a032dc049
Aditya Rao <adityarao2005@gmail.com>
2024-01-13 22:53:29 -0500

    updated backend

--- quake-guard/quake-guard-frontend/src/app/donate/page.tsx
+++ quake-guard/quake-guard-frontend/src/app/donate/page.tsx
@@ -1,5 +1,27 @@
 import React from "react";
 
-export default function DonatePage() {
-	return <div></div>;
+export default function AboutPage() {
+	return (
+		<div className="border border-black rounded">
+			<div className="make-box">
+				<h1>Sign up</h1>
+				<form>
+					<div className="input-group">
+						<div className="input-field">
+							<i className="fa-solid fa_user"></i>
+							<input type="text" placeholder="Name" />
+						</div>
+						<div className="input-field">
+							<i className="fa-solid fa_envelope"></i>
+							<input type="email" placeholder="Email" />
+						</div>
+						<div className="input-field">
+							<i className="fa-solid fa_lock"></i>
+							<input type="password" placeholder="Password" />
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
