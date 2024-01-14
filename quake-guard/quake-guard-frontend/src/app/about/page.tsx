6feed0b81ebab691859b31ece81c3d869b16d686
taeyeonha <taeyeonha10@gmail.com>
2024-01-14 07:49:23 -0500

    Fixed everything before Aditya

--- quake-guard/quake-guard-frontend/src/app/about/page.tsx
+++ quake-guard/quake-guard-frontend/src/app/about/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AboutPage() {
 	return (
 		<div className='flex-auto grid grid-cols-2 gap-4 bg-gradient-to-b from-slate-300 from-10% from-stone-100 via-30% to-white to-90% ..."'>
-			<div>
+			<div className = "animate-slideInLeft">
 				<div className='bg-cover my-20 h-3/6 w-5/6 ml-24 rounded-2xl bg-[url("/quake3.jpeg")]'></div>
 				<h1 className='text-3xl font-bold ml-24 mb-4 p-3 underline'>
 					The Solution
@@ -12,7 +12,7 @@ export default function AboutPage() {
 				<div>
 					<p className="font-bold ml-24 p-3 pb-8">So... How are we <u>DIFFERENT</u>???</p>
 				</div>
-				<div>
+				<div className = "animate-slideInLeft">
 					<p className='font-bold ml-24 px-2'>
 						1. AI and Machine Learning Powered
 					</p>
@@ -20,7 +20,7 @@ export default function AboutPage() {
 					Uses <strong>AI</strong> and <strong>Machine Learning</strong> to predict earthquakes with higher accuracy, enabling timely and informed decisions by authorities and communities!
 					</p>
 				</div>
-				<div>
+				<div className = "animate-slideInLeft">
 					<p className='font-bold ml-24 px-2'>
 						2. Global Community & Support
 					</p>
@@ -35,15 +35,17 @@ export default function AboutPage() {
 					</p>
 				</div>
 			</div>
-			<div className='flex-1 p-4'>
-				<div className='pb-4 p-4'>
-					<h1 className='text-3xl font-bold py-4 ml-6 mt-1 underline'>
+			<div className='animate-slideInRight'>
+				<div className='flex-1 p-4'>
+					<div className='pb-4 p-4'>
+						<h1 className='text-3xl font-bold py-4 ml-6 mt-1 underline'>
 						The Product
-					</h1>
+						</h1>
 					<p className='py-4 leading-relaxed ml-6 mr-24'>
 					As condos and urban living are increasing, dangers due to earthquakes continue to rise. As such, the need for a proactive and data-driven earthquake risk prediction has never been more critical. Our initiative, <strong><i>QuakeGuard</i></strong>, harnesses <strong>Machine Learning</strong> capabilities to revolutionize earthquake risk assessment. <i><strong>QuakeGuard</strong></i> is committed to providing information to empowering authorities, communities, and first responders with actionable insights for resource allocation and prevention by geographical, longitudinal, and latitudinal analysis.
 					</p>
 				</div>
+			</div>
 				<div className='pb-4 p-4 leading-relaxed'>
 					<h1 className='text-3xl font-bold ml-6 underline'>
 						The Inspiration
