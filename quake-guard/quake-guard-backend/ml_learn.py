063bea741098b0a76386f86c41e6a177d64294f6
Aditya Rao <adityarao2005@gmail.com>
2024-01-26 00:12:13 -0500

    worked on all possible types of ML and found my best one (neural network)

--- quake-guard/quake-guard-backend/ml_learn.py
+++
@@ -1,9 +0,0 @@
-# import statements
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# import the dataset
-data = pd.read_csv('silver.csv')
-# describe dataset
-data.describe()
