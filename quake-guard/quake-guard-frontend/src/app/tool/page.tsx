9e7fc61b89064c86b1b879ab72490b5cd53d23a5
Aditya Rao <adityarao2005@gmail.com>
2024-02-04 02:50:33 -0500

    updated the boxes

--- quake-guard/quake-guard-frontend/src/app/tool/page.tsx
+++ quake-guard/quake-guard-frontend/src/app/tool/page.tsx
@@ -17,20 +17,16 @@ const PickTwoScreen: React.FC = () => {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen bg-[url('/mountains.jpg')]">
 			<div className='flex-1 flex'>
-				<a href='/tool/graph'>
-					<div
-						className='bg-gray-300 rounded-lg p-6 m-20 flex-1 flex flex-col items-center justify-center shadow-2xl backdrop-blur-md bg-white/30'
-					>
+				<a href='/tool/graph' className='flex-1 flex'>
+					<div className='bg-gray-300 rounded-lg p-6 m-20 flex-1 flex flex-col items-center justify-center shadow-2xl backdrop-blur-md bg-white/30'>
 						<h1 className='text-3xl font-bold text-center'>
 							Go to Map
 						</h1>
 						<img src='/compass2.png' width='300' height='400' />
 					</div>
 				</a>
-				<a href='/tool/predict'>
-					<div
-						className='bg-gray-300 rounded-lg p-6 m-20 flex-1 flex flex-col items-center justify-center shadow-2xl backdrop-blur-md bg-white/30'
-					>
+				<a href='/tool/predict' className='flex-1 flex'>
+					<div className='bg-gray-300 rounded-lg p-6 m-20 flex-1 flex flex-col items-center justify-center shadow-2xl backdrop-blur-md bg-white/30'>
 						<h1 className='text-3xl font-bold text-center pb-2'>
 							Check Earthquake Data
 						</h1>
