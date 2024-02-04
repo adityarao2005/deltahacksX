f0c66d241b47a064ca04c90a5937bda72047dd86
shaunp18 <shaunplassery@gmail.com>
2024-02-04 02:32:12 -0500

    Made two part system

--- quake-guard/quake-guard-frontend/src/app/tool/page.tsx
+++ quake-guard/quake-guard-frontend/src/app/tool/page.tsx
@@ -10,9 +10,47 @@ enum LoadingState {
 	LOADING,
 	LOADED,
 }
-
+const PickTwoScreen: React.FC = () => {
+	const [showToolPage, setShowToolPage] = useState(false);
+  
+	const handleGoToMapClick = () => {
+	  window.location.href = '/tool/graph';
+	};
+  
+	const handleCheckEarthquakeDataClick = () => {
+	  setShowToolPage(true);
+	};
+  
+	return (
+	  <div className="flex flex-col items-center justify-center h-screen bg-[url('/mountains.jpg')]">
+		{showToolPage ? (
+		  <ToolPage />
+		) : (
+		  <div className="flex-1 flex">
+			<div
+			  onClick={handleGoToMapClick}
+			  className="bg-gray-300 rounded-lg p-6 m-20 flex-1 flex flex-col items-center justify-center shadow-2xl backdrop-blur-md bg-white/30"
+			>
+			  
+			  <h1 className="text-3xl font-bold text-center">Go to Map</h1>
+			  <img src = "/compass2.png" width = "300" height = "400" /> 
+			</div>
+			<div
+			  onClick={handleCheckEarthquakeDataClick}
+			  className="bg-gray-300 rounded-lg p-6 m-20 flex-1 flex flex-col items-center justify-center shadow-2xl backdrop-blur-md bg-white/30"
+			>
+			  <h1 className="text-3xl font-bold text-center pb-2">Check Earthquake Data</h1>
+			  <img src = "/magnifying.png" width = "250" height = "250"/> 
+			</div>
+		  </div>
+		)}
+	  </div>
+	);
+  };
+  
+  export default PickTwoScreen;
 // Components
-export default function ToolPage() {
+function ToolPage() {
 	// City, State, Country
 	const [city, setCity] = useState("");
 	const [state, setState] = useState("");
